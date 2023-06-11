fd6cb50aa9eb49e577bbb77a33f740e52ff700ef
Anic <Anic@DESKTOP-E2N5CQ7>
2023-06-11 16:16:41 +0200

    Donut odradjen i raspored dugmica lepse rasporedjen

---
+++ StarterProject/src/drawing/DlgDonut.java
@@ -0,0 +1,195 @@
+package drawing;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+import geometry.Donut;
+import geometry.Point;
+import geometry.Rectangle;
+
+public class DlgDonut extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	private JTextField txtX;
+	private JTextField txtY;
+	private JTextField txtRadius;
+	private JTextField txtInnerRadius;
+	private boolean isOk = false;
+	
+	private Donut donut;
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			DlgDonut dialog = new DlgDonut();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public DlgDonut() {
+		setModal(true);
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		GridBagLayout gbl_contentPanel = new GridBagLayout();
+		gbl_contentPanel.columnWidths = new int[] {60, 40, 40};
+		gbl_contentPanel.rowHeights = new int[] {30, 0, 30, 0, 30, 0, 0, 30, 30, 0};
+		gbl_contentPanel.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+		gbl_contentPanel.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+		contentPanel.setLayout(gbl_contentPanel);
+		{
+			JLabel lblX = new JLabel("X:");
+			GridBagConstraints gbc_lblX = new GridBagConstraints();
+			gbc_lblX.insets = new Insets(0, 0, 5, 5);
+			gbc_lblX.anchor = GridBagConstraints.EAST;
+			gbc_lblX.gridx = 0;
+			gbc_lblX.gridy = 3;
+			contentPanel.add(lblX, gbc_lblX);
+		}
+		{
+			txtX = new JTextField();
+			GridBagConstraints gbc_txtX = new GridBagConstraints();
+			gbc_txtX.insets = new Insets(0, 0, 5, 0);
+			gbc_txtX.fill = GridBagConstraints.HORIZONTAL;
+			gbc_txtX.gridx = 1;
+			gbc_txtX.gridy = 3;
+			contentPanel.add(txtX, gbc_txtX);
+			txtX.setColumns(10);
+		}
+		{
+			JLabel lblY = new JLabel("Y:");
+			GridBagConstraints gbc_lblY = new GridBagConstraints();
+			gbc_lblY.anchor = GridBagConstraints.EAST;
+			gbc_lblY.insets = new Insets(0, 0, 5, 5);
+			gbc_lblY.gridx = 0;
+			gbc_lblY.gridy = 4;
+			contentPanel.add(lblY, gbc_lblY);
+		}
+		{
+			txtY = new JTextField();
+			GridBagConstraints gbc_txtY = new GridBagConstraints();
+			gbc_txtY.insets = new Insets(0, 0, 5, 0);
+			gbc_txtY.fill = GridBagConstraints.HORIZONTAL;
+			gbc_txtY.gridx = 1;
+			gbc_txtY.gridy = 4;
+			contentPanel.add(txtY, gbc_txtY);
+			txtY.setColumns(10);
+		}
+		{
+			JLabel lblRadius = new JLabel("Radius:");
+			GridBagConstraints gbc_lblRadius = new GridBagConstraints();
+			gbc_lblRadius.anchor = GridBagConstraints.EAST;
+			gbc_lblRadius.insets = new Insets(0, 0, 5, 5);
+			gbc_lblRadius.gridx = 0;
+			gbc_lblRadius.gridy = 5;
+			contentPanel.add(lblRadius, gbc_lblRadius);
+		}
+		{
+			txtRadius = new JTextField();
+			GridBagConstraints gbc_txtRadius = new GridBagConstraints();
+			gbc_txtRadius.insets = new Insets(0, 0, 5, 0);
+			gbc_txtRadius.fill = GridBagConstraints.HORIZONTAL;
+			gbc_txtRadius.gridx = 1;
+			gbc_txtRadius.gridy = 5;
+			contentPanel.add(txtRadius, gbc_txtRadius);
+			txtRadius.setColumns(10);
+		}
+		{
+			JLabel lblInnerRadius = new JLabel("Inner radius:");
+			GridBagConstraints gbc_lblInnerRadius = new GridBagConstraints();
+			gbc_lblInnerRadius.anchor = GridBagConstraints.EAST;
+			gbc_lblInnerRadius.insets = new Insets(0, 0, 5, 5);
+			gbc_lblInnerRadius.gridx = 0;
+			gbc_lblInnerRadius.gridy = 6;
+			contentPanel.add(lblInnerRadius, gbc_lblInnerRadius);
+		}
+		{
+			txtInnerRadius = new JTextField();
+			GridBagConstraints gbc_txtInnerRadius = new GridBagConstraints();
+			gbc_txtInnerRadius.insets = new Insets(0, 0, 5, 0);
+			gbc_txtInnerRadius.fill = GridBagConstraints.HORIZONTAL;
+			gbc_txtInnerRadius.gridx = 1;
+			gbc_txtInnerRadius.gridy = 6;
+			contentPanel.add(txtInnerRadius, gbc_txtInnerRadius);
+			txtInnerRadius.setColumns(10);
+		}
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton okButton = new JButton("OK");
+				okButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						int x = Integer.parseInt(txtX.getText());
+						int y= Integer.parseInt(txtY.getText());
+						int radius = Integer.parseInt(txtRadius.getText());
+						int innerRadius = Integer.parseInt(txtInnerRadius.getText());
+						donut = new Donut(new Point(x,y), radius, innerRadius);
+						isOk=true;
+						dispose();
+					}
+				});
+				okButton.setActionCommand("OK");
+				buttonPane.add(okButton);
+				getRootPane().setDefaultButton(okButton);
+			}
+			{
+				JButton cancelButton = new JButton("Cancel");
+				cancelButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						isOk =false;
+						dispose();
+					}
+				});
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+	}
+
+	public JTextField getTxtX() {
+		return txtX;
+	}
+
+	public JTextField getTxtY() {
+		return txtY;
+	}
+
+	public JTextField getTxtRadius() {
+		return txtRadius;
+	}
+
+	public JTextField getTxtInnerRadius() {
+		return txtInnerRadius;
+	}
+
+	public boolean isOk() {
+		return isOk;
+	}
+	public Donut getDonut()
+	{
+		return this.donut;
+	}
+
+}
--- StarterProject/src/drawing/FrmDraw.java
+++ StarterProject/src/drawing/FrmDraw.java
@@ -7,6 +7,7 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
 import geometry.Circle;
+import geometry.Donut;
 import geometry.Line;
 import geometry.Point;
 import geometry.Rectangle;
@@ -25,6 +26,7 @@ import java.awt.Color;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.ButtonGroup;
+import java.awt.Font;
 
 public class FrmDraw extends JFrame {
 
@@ -36,6 +38,7 @@ public class FrmDraw extends JFrame {
 	private Point startPoint;
 	private Rectangle rectangle;
 	private Circle circle;
+	private Donut donut;
 	/**
 	 * Launch the application.
 	 */
@@ -65,25 +68,33 @@ public class FrmDraw extends JFrame {
 		contentPane.setLayout(new BorderLayout(0, 0));
 		
 		JPanel panel = new JPanel();
+		FlowLayout flowLayout = (FlowLayout) panel.getLayout();
+		flowLayout.setAlignment(FlowLayout.RIGHT);
+		flowLayout.setHgap(54);
 		contentPane.add(panel, BorderLayout.NORTH);
 		
 		JToggleButton tglbtnPoint = new JToggleButton("Point");
+		tglbtnPoint.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		buttonGroup.add(tglbtnPoint);
 		panel.add(tglbtnPoint);
 		
 		JToggleButton tglbtnLine = new JToggleButton("Line");
+		tglbtnLine.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		buttonGroup.add(tglbtnLine);
 		panel.add(tglbtnLine);
 		
 		JToggleButton tglbtnRectangle = new JToggleButton("Rectangle");
+		tglbtnRectangle.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		buttonGroup.add(tglbtnRectangle);
 		panel.add(tglbtnRectangle);
 		
 		JToggleButton tglbtnCircle = new JToggleButton("Circle");
+		tglbtnCircle.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		buttonGroup.add(tglbtnCircle);
 		panel.add(tglbtnCircle);
 		
 		JToggleButton tglbtnDonut = new JToggleButton("Donut");
+		tglbtnDonut.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		buttonGroup.add(tglbtnDonut);
 		panel.add(tglbtnDonut);
 		
@@ -91,41 +102,45 @@ public class FrmDraw extends JFrame {
 		contentPane.add(panel_1, BorderLayout.WEST);
 		GridBagLayout gbl_panel_1 = new GridBagLayout();
 		gbl_panel_1.columnWidths = new int[]{115, 0};
-		gbl_panel_1.rowHeights = new int[]{21, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_panel_1.rowHeights = new int[] {0, 0, 0};
 		gbl_panel_1.columnWeights = new double[]{0.0, Double.MIN_VALUE};
-		gbl_panel_1.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel_1.rowWeights = new double[]{1.0, 1.0, 1.0, 1.0}; 
 		panel_1.setLayout(gbl_panel_1);
 		
 		JToggleButton tglbtnDraw = new JToggleButton("Draw");
+		tglbtnDraw.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		buttonGroup_1.add(tglbtnDraw);
 		GridBagConstraints gbc_tglbtnDraw = new GridBagConstraints();
 		gbc_tglbtnDraw.insets = new Insets(0, 0, 5, 0);
-		gbc_tglbtnDraw.anchor = GridBagConstraints.NORTH;
 		gbc_tglbtnDraw.gridx = 0;
-		gbc_tglbtnDraw.gridy = 3;
+		gbc_tglbtnDraw.gridy = 0;
 		panel_1.add(tglbtnDraw, gbc_tglbtnDraw);
 		
 		JToggleButton tglbtnSelect = new JToggleButton("Select");
+		tglbtnSelect.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		buttonGroup_1.add(tglbtnSelect);
 		GridBagConstraints gbc_tglbtnSelect = new GridBagConstraints();
 		gbc_tglbtnSelect.insets = new Insets(0, 0, 5, 0);
 		gbc_tglbtnSelect.gridx = 0;
-		gbc_tglbtnSelect.gridy = 6;
+		gbc_tglbtnSelect.gridy = 1;
 		panel_1.add(tglbtnSelect, gbc_tglbtnSelect);
 		
 		JToggleButton tglbtnModify = new JToggleButton("Modify");
+		tglbtnModify.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		buttonGroup_1.add(tglbtnModify);
 		GridBagConstraints gbc_tglbtnModify = new GridBagConstraints();
 		gbc_tglbtnModify.insets = new Insets(0, 0, 5, 0);
 		gbc_tglbtnModify.gridx = 0;
-		gbc_tglbtnModify.gridy = 9;
+		gbc_tglbtnModify.gridy = 2;
 		panel_1.add(tglbtnModify, gbc_tglbtnModify);
 		
 		JToggleButton tglbtnDelete = new JToggleButton("Delete");
+		tglbtnDelete.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		buttonGroup_1.add(tglbtnDelete);
 		GridBagConstraints gbc_tglbtnDelete = new GridBagConstraints();
+		gbc_tglbtnDelete.insets = new Insets(0, 0, 5, 0);
 		gbc_tglbtnDelete.gridx = 0;
-		gbc_tglbtnDelete.gridy = 12;
+		gbc_tglbtnDelete.gridy = 3;
 		panel_1.add(tglbtnDelete, gbc_tglbtnDelete);
 		
 		PnlDrawing pnl = new PnlDrawing();
@@ -199,7 +214,19 @@ public class FrmDraw extends JFrame {
 							pnl.addShape(circle);
 						}
 					}
-				
+					if(tglbtnDonut.isSelected())
+					{
+						DlgDonut dialog = new DlgDonut();
+						 dialog.getTxtX().setText(String.valueOf(x));
+						 dialog.getTxtY().setText(String.valueOf(y));
+						 dialog.setVisible(true);
+						 
+						 if(dialog.isOk())
+						 {
+							 donut = dialog.getDonut();
+							 pnl.addShape(donut);
+						 }
+					}
 					
 				}
 			}
